1e829f8b9e80e338cbaa5198afac565fa0b2aa64
Manasa patil <manasapatil226@gmail.com>
2025-01-25 18:48:58 +0530

    just checking

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Thanushree.P <code>src/App.js</code> and save to reload.
+         Thanushree.P and Manasa.p <code>src/App.js</code> and save to reload.
         </p>
         <h1>Welcome to Patient-Management Project</h1>
         
