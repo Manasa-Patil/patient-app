7992f8150d367e240a81a5e450ab39f9fe3806bf
ThanushreeP <thanu22052005@gmail.com>
2025-02-01 17:44:42 +0530

    Login SetUp

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './Components/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Thanushree.P <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Welcome to Patient-Management Project</h1>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <Login />
+  </div>;
 }
 
 export default App;
