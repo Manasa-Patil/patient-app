3a5050df11620f7e557021bcfc0fbfa0d7cf24b8
Manasa patil <manasapatil226@gmail.com>
2025-02-01 21:01:59 +0530

    Added forms

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -74,11 +74,15 @@ export default function Login() {
        <h2>Login</h2>
       <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">First Name</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="First Name"/>
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="First Name"/>
 </div>
       <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Last Name</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Last Name"/>
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Last Name"/>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Phone Number</label>
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Phone Number"/>
 </div>
       <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Email address</label>
@@ -90,10 +94,38 @@ export default function Login() {
   Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
 </div>
 <div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label"> Enter your Symptoms</label>
+<select class="form-select" aria-label="Default select example">
+  <option selected>Select Location</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+  <option value="3">Three</option>
+  <option value="3">Three</option>
+  <option value="3">Three</option>
+</select>
+</div>
+<div class="mb-3">
+<select class="form-select" aria-label="Default select example">
+  <option selected>Select Speciality</option>
+  <option value="1">Gyneacologist</option>
+  <option value="2">Surgeon</option>
+  <option value="3">Orthopedic</option>
+  <option value="3">Pediatrician</option>
+  <option value="3">Physicist</option>
+  <option value="3">Dermatologist</option>
+</select>
+</div>
+
+
+
+
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Message</label>
   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 </div>
-<label for="exampleDataList" class="form-label">Doctors</label>
+<button type="button" class="btn btn-primary">Submit</button>
+<button type="button" class="btn btn-link">Back</button>
+{/* <label for="exampleDataList" class="form-label">Doctors</label>
 <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..."/>
 <datalist id="datalistOptions">
   <option value="Gyneacologist"/>
@@ -102,7 +134,7 @@ export default function Login() {
   <option value="Pediatrician"/>
   <option value="Physicist"/>
   <option value="Dermatologist"/>
-</datalist>
+</datalist> */}
     </div>
   );
 }
