eb88a5e6b9e6a0c7070aee4fafbebd0dde183e96
ThanushreeP <thanu22052005@gmail.com>
2025-02-01 18:06:28 +0530

    Login-SetUp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import Login from './Components/Login';
 
 function App() {
+<<<<<<< HEAD
   return <div>
     <Login />
   </div>;
+=======
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> parent of cd65e44 (Merge pull request #1 from PThanushree/branch-tp)
 }
 
 export default App;
